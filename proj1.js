7405546253315863ca5dee5db5db7ad0c5e86f12
Constantin Koehler <constantin@backpackem.com>
2017-03-27 13:33:57 -0400

    Change File Extensions

--- proj1.js
+++ proj1.js
@@ -2,14 +2,13 @@ EASYDIFFICULTY = "EASY";
 MEDIUMDIFFICULTY = "MEDIUM";
 HARDDIFFICULTY = "HARD";
 
-EASYCLOCKS = ["120.jpg","215.jpg","230.jpg","255.png","355.jpg","515.png","540.jpg","640.jpg,","745.jpg","1025.jpg"];
+EASYCLOCKS = ["120.png","215.png","230.png","255.png","355.png","515.png","540.png","640.png,","745.png","1025.png"];
 MEDIUMCLOCKS = ["250.png","325.png","615.png","720.png","835.png","940.png","1000.png","1035.png","1120.png","1205.png"]
 HARDCLOCKS = ["219.png", "303.png", "317.png", "514.png", "657.png", "713.png", "833.png", "842.png", "909.png", "1152.png"]
 
 NUMQUESTIONS = 10;
 
 difficulties = [EASYDIFFICULTY,MEDIUMDIFFICULTY,HARDDIFFICULTY];
-
 
 lifeTotal = 3;
 
@@ -39,6 +38,14 @@ function runGame(difficultyIndex){
   x.setAttribute("height", "220");
   console.log(clockDiv);
   clockDiv.appendChild(x);
+
+  var guess;
+
+  setTimeout(function(){
+    guess = prompt("What Time is It?");
+  }, 500);
+
+
 
   // while(lifeTotal != 0){
   //   var number = Math.floor((Math.random() * 10) + 1);
